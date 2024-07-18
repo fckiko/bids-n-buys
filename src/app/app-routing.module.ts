439c6a107b695cb16d4f780a39191eb5b8fe39ef
Francis <francis@serverless-secure.com-at-025181935248>
2024-07-18 12:52:39 +0800

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,15 @@ import { RouterModule, Routes, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { Meta, Title } from '@angular/platform-browser'
 import { BiddingPageComponent } from './bidding-page/bidding-page.component';
+import { OfferingPageComponent } from './offering-page/offering-page.component';
+import { BuyingPageComponent } from './buying-page/buying-page.component';
 
 
 const routes: Routes = [
   // Define your routes here
-  {path:'bidding-page', component:BiddingPageComponent}
+  {path:'bidding-page', component:BiddingPageComponent},
+  {path:'offering-page', component:OfferingPageComponent},
+  {path:'buying-page', component:BuyingPageComponent}
 ];
 
 
